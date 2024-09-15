5ef939fb8d34818fe3729040b288444fc453f672
Hongji1001 <adolfxuhj@163.com>
2024-09-14 20:36:03 -0400

    add some feature

--- src/test_lib.py
+++ src/test_lib.py
@@ -1,5 +1,5 @@
 from pandas import DataFrame
-from lib import visualization, generate_pdf, read_dataset, _file_exists_and_not_empty
+from lib import visualization, generate_pdf, read_dataset, calculate_statistics, _file_exists_and_not_empty
 
 df = read_dataset("../NBA_2021.csv")
 
@@ -7,6 +7,12 @@ df = read_dataset("../NBA_2021.csv")
 def test_read_dataset():
     assert isinstance(df, DataFrame)
     assert len(df) > 0
+    
+
+def test_calculate_statistics():
+    assert calculate_statistics(df, "PTS")[1] == 7.2
+    assert calculate_statistics(df, "BLK")[1] == 0.3
+    assert calculate_statistics(df, "AST")[1] == 1.4
 
 
 def test_visualization():
